ca8123f9be72f2fc787c00f05352bc3f2c4a68af
Anderson <anderson.araf@gmail.com>
2021-07-17 20:48:46 -0500

    hover is css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import Tmdb from './Tmdb'
+import MovieRow from './components/MoviewRow'
 
-export default () => {
-  const [movieList, setMovieList] = useState([]) 
+export default  () => {
+  const [movieList, setMovieList] = useState([])
 
   useEffect(() => {
     const loadAll = async () => {
       //PEGANDO A LISTA TOTAAL
-      let list = await Tmdb.getHomeList() 
+      let list = await Tmdb.getHomeList()
       setMovieList(list)
     }
     loadAll()
@@ -15,9 +17,11 @@ export default () => {
 
   return (
     <div className="page">
-      Header
-      Destaque
-      Lista
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   )
 }
